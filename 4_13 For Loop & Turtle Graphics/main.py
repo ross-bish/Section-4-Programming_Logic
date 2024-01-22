23b2ab49cf2b1cde9977095283cd5d6057afd410
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 20:46:46 +0000

    Add files via upload

---
+++ 4_13 For Loop & Turtle Graphics/main.py
@@ -0,0 +1,27 @@
+# -------------------------
+# Title: 
+# Date:
+# -------------------------
+
+import turtle
+
+### Add comments to separate each of the Tasks as necessary.
+
+
+
+
+# Task 1
+
+
+# Task 2
+
+
+# Task 3
+
+
+
+
+
+  
+
+
