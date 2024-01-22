31294f86d43c6cbda9a4d1a93db94fa4d01393de
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 20:45:09 +0000

    Add files via upload

---
+++ 4_12 For Loops/main.py
@@ -0,0 +1,40 @@
+# ----------------------------
+# Title: For Loops
+# Date: 
+# ----------------------------
+
+
+# 1 - Use comments where necessary in your code
+
+# 2 - Please number and comment out your solutions  
+'''
+### Challenge 1
+name = input("Enter your name: ")
+
+for i in range(3):
+  print(name)
+
+
+### Challenge 2
+name  = input("Enter your name: ")
+number = int(input("Enter a number: "))
+
+for i in range (number):
+  print(name)
+
+
+### Challenge 3
+name  = input("Enter your name: ")
+
+for i in name:
+  print(i)
+'''
+
+### Extra Credit
+number = int(input("Please enter a number between 1 - 12: "))
+
+for i in range (1, 13):
+  print(i, "x", number, "=", i*number)
+
+
+
