dd429dd60d8d81259d6188cfa40462c387ac1b53
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 20:40:09 +0000

    Update main.py

--- 4.9 While Loops (Textbook Tasks Q1-3)/main.py
+++ 4.9 While Loops (Textbook Tasks Q1-3)/main.py
@@ -6,47 +6,16 @@
 
 
 ### Task 1
-# task 1
-'''
-counter = 1000
-total = 0
 
-while(counter <= 1500):
-  print(counter)
-  counter+=1
-  total = total + counter
-
-print("Total: ", total)
 
 
 
 ### Task 2
-counter = 0
-total = 0
 
-while counter < 6:
-  numbers = int(input("Enter a number: "))
-  counter += 1
-  total += numbers
 
-print("The six numbers total: ",total, "the average is: ", total/6)
-'''
 
 
 ### Task 3
-username = input("Enter Username: ")
-
-while username != "Maureen":
-  username = input("Wrong username - re-enter username: ")
-
-print("Thank you ", username)
-
-password = input("Enter Password: ")
-
-while password != "abc123":
-  password = input("Wrong password - re-enter password: ")
-  
-print("Access Granted!")
 
 
 
