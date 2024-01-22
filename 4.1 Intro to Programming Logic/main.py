2f701e7c2005ce41c7977e9475815cceabc00086
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 20:00:59 +0000

    Add files via upload

---
+++ 4.1 Intro to Programming Logic/main.py
@@ -0,0 +1,13 @@
+# ------------------------
+# Title: Programming Logic
+# Date: 
+# ------------------------
+
+# initialise variables x, y and z
+
+# initialise variables x, y and z
+x = 1
+y = 0
+z = -1
+
+print(6 >= 5)
