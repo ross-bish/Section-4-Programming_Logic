4b8e33d4dd7d124ac4fe926f2e96277a70b9448b
Ross Conboy <83789503+ross-bish@users.noreply.github.com>
2024-01-22 20:35:04 +0000

    Update main.py

--- 4.6 Guessing Game/main.py
+++ 4.6 Guessing Game/main.py
@@ -3,23 +3,3 @@
 # Date: 
 # ------------------------
 
-import random
-
-number = random.randint(1,10)
-guess = 0
-
-guess = int(input("Pick a number: "))
-
-while guess != number:
-  if guess < number:
-    print("Too low")
-   
-  elif guess > number:
-    print("Too high")
-    
-  else:
-    print("Correct")
-    break
-  guess = int(input("Pick a number: "))
-
-print("The number was", number) # shows random number selected
